b85c908f6dc2383ecd51173409abf8b0e42b6af4
MuhammadSaadat\Muhammad Saadat <muhammadsaadat42@gmail.com>
2018-02-20 21:11:09 +0500

    This tutorial is for dragging items in UI.
    In inventory we often want to drag and drop items (basically want to replace them). This script teaches us how to do that.

---
+++ Drag And Drop Tutorial/Assets/Scripts/UIElementDragger.cs
@@ -0,0 +1,98 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIElementDragger : MonoBehaviour {
+
+    public const string DRAGGABLE_TAG = "UIDraggable";
+
+    private bool dragging = false;
+
+    private Vector2 originalPosition;
+
+    private Transform objectToDrag;
+    private Image objectToDragImage;
+
+    List<RaycastResult> hitObjects = new List<RaycastResult>();
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            objectToDrag = GetDraggableTransformUnderMouse();
+
+            if (objectToDrag != null)
+            {
+                dragging = true;
+                objectToDrag.SetAsLastSibling();
+
+                originalPosition = objectToDrag.position;
+                objectToDragImage = objectToDrag.GetComponent<Image>();
+                objectToDragImage.raycastTarget = false;
+            }
+
+        }
+
+        if (dragging)
+        {
+            if(objectToDrag != null)
+                objectToDrag.position = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (objectToDrag != null)
+            {
+                Transform objectToReplace = GetDraggableTransformUnderMouse();
+
+                if (objectToReplace != null)
+                {
+                    objectToDrag.position = objectToReplace.position;
+                    objectToReplace.position = originalPosition;
+                }
+                else
+                {
+                    objectToDrag.position = originalPosition;
+                }
+
+                objectToDragImage.raycastTarget = true;
+                objectToDrag = null;
+
+            }
+
+            dragging = false;
+        }
+
+    }
+
+    private GameObject GetObjectUnderMouse()
+    {
+        PointerEventData pointer = new PointerEventData(EventSystem.current);
+
+        pointer.position = Input.mousePosition;
+
+        EventSystem.current.RaycastAll(pointer, hitObjects);
+
+        if (hitObjects.Count <= 0)
+            return null;
+
+        return hitObjects.First().gameObject;
+    }
+
+    private Transform GetDraggableTransformUnderMouse()
+    {
+        GameObject clickedObject = GetObjectUnderMouse();
+
+        if (clickedObject != null && clickedObject.tag == DRAGGABLE_TAG)
+        {
+            return clickedObject.transform;
+        }
+
+        return null;
+
+    }
+
+}
